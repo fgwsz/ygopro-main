32e8e00fe7c1ce8773e2bda4ba520b191dddb75d
fgwsz <3191000458@qq.com>
2024-07-02 01:47:28 +0800

    add download ygopro

--- update-ygopro.sh
+++ update-ygopro.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 ygopro_filename="KoishiPro-master-linux-zh-CN.tar.gz"
-tar -zxvf "./install/$ygopro_filename" -C ./ygopro-ocg/
-tar -zxvf "./install/$ygopro_filename" -C ./ygopro-408/
+ygopro_download_url="https://cdn02.moecube.com:444/koishipro/archive/$ygopro_filename"
+ygopro_path="./install/$ygopro_filename"
+ygopro_old_path="./install/${ygopro_filename}.old"
+
+if [[ -e "$ygopro_path" ]]; then
+    mv "$ygopro_path" "$ygopro_old_path"
+fi
+curl -o "$ygopro_path" "$ygopro_download_url"
+tar -zxvf "$ygopro_path" -C ./ygopro-ocg/
+tar -zxvf "$ygopro_path" -C ./ygopro-408/
 ./update-ext.sh
 rm -rf ./ygopro-ocg/deck/*
 rm -rf ./ygopro-408/deck/*
