54d7b1c889626b3e48b2f4965c7d8df724b819db
fgwsz <3191000458@qq.com>
2024-07-02 00:39:58 +0800

    clean

--- update-ygopro.sh
+++ update-ygopro.sh
@@ -3,4 +3,6 @@ ygopro_filename="KoishiPro-master-linux-zh-CN.tar.gz"
 tar -zxvf "./install/$ygopro_filename" -C ./ygopro-ocg/
 tar -zxvf "./install/$ygopro_filename" -C ./ygopro-408/
 ./update-ext.sh
+rm -rf ./ygopro-ocg/deck/*
+rm -rf ./ygopro-408/deck/*
 ./update-deck.sh
