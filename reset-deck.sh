3fb1efdebc8f3f8bec2facdf02c7b29d4af2568a
fgwsz <3191000458@qq.com>
2024-07-02 02:20:40 +0800

    impl reset-deck

--- reset-deck.sh
+++ reset-deck.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+deck_root_path="./install/"
+deck_repo_name="ygopro-deck"
+deck_path="${deck_root_path}${deck_repo_name}"
+
+if [[ ! -e "$deck_path" ]]; then
+    cd "$deck_root_path"
+    git clone "git@github.com:fgwsz/$deck_repo_name"
+    cd ..
+fi
+cd  "$deck_path"
+git pull
+cd ../..
 rm -rf ./ygopro-ocg/deck/*
 rm -rf ./ygopro-408/deck/*
-./update-deck.sh
+cp -r "${deck_path}" ./ygopro-ocg/deck/
+cp -r "${deck_path}" ./ygopro-408/deck/
--- update-deck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ./install/ygopro-deck/
-git pull
-cd ../..
-cp -r ./install/ygopro-deck/* ./ygopro-ocg/deck/
-cp -r ./install/ygopro-deck/* ./ygopro-408/deck/
