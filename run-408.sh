55080fac889bf3d289881cab78374eb78a2bd08d
fgwsz <3191000458@qq.com>
2024-07-04 09:24:51 +0800

    auto update deck with run

--- run-408.sh
+++ run-408.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#pull deck
+cd ./ygopro-408/deck
+git pull
+cd ../..
+#run ygopro
 cd ./ygopro-408
 ./ygopro
 cd ..
+#push deck
+cd ./ygopro-408/deck
+./push-deck.sh
+cd ../..
--- run-ocg.sh
+++ run-ocg.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#pull deck
+cd ./ygopro-ocg/deck
+git pull
+cd ../..
+#run ygopro
 cd ./ygopro-ocg
 ./ygopro
 cd ..
+#push deck
+cd ./ygopro-ocg/deck
+./push-deck.sh
+cd ../..
