a25080569f642651a61f3ebbb196ef0872dab99f
fgwsz <3191000458@qq.com>
2024-08-07 14:59:04 +0800

    add download deck repo

--- run-408.sh
+++ run-408.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
+#download ygopro-deck git repo
+ygopro_408_path="$root_path/ygopro-408"
+deck_path="$ygopro_408_path/deck"
+if [[ ! -e "$deck_path/.git" ]]; then
+    rm -rf "$deck_path"
+    cd "$ygopro_408_path"
+    git clone "git@github.com:fgwsz/ygopro-deck.git"
+    mv "$ygopro_408_path/ygopro-deck" "$ygopro_408_path/deck"
+fi
 #pull deck
-cd "$root_path/ygopro-408/deck"
+cd "$deck_path"
 git pull
 #run ygopro
-cd "$root_path/ygopro-408"
+cd "$ygopro_408_path"
 ./ygopro
 #push deck
-"$root_path/ygopro-408/deck/push-deck.sh"
+"$deck_path/push-deck.sh"
--- run-ocg.sh
+++ run-ocg.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
+#download ygopro-deck git repo
+ygopro_ocg_path="$root_path/ygopro-ocg"
+deck_path="$ygopro_ocg_path/deck"
+if [[ ! -e "$deck_path/.git" ]]; then
+    rm -rf "$deck_path"
+    cd "$ygopro_ocg_path"
+    git clone "git@github.com:fgwsz/ygopro-deck.git"
+    mv "$ygopro_ocg_path/ygopro-deck" "$ygopro_ocg_path/deck"
+fi
 #pull deck
-cd "$root_path/ygopro-ocg/deck"
+cd "$deck_path"
 git pull
 #run ygopro
-cd "$root_path/ygopro-ocg"
+cd "$ygopro_ocg_path"
 ./ygopro
 #push deck
-"$root_path/ygopro-ocg/deck/push-deck.sh"
+"$deck_path/push-deck.sh"
