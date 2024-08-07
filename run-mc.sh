e8fb00cd5ed113405d3fcd68f1ef293818e487fa
fgwsz <3191000458@qq.com>
2024-08-07 15:50:33 +0800

    add mc

---
+++ run-mc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+root_path=$(dirname "$(readlink -f "$0")")
+ygopro_mc_path=~/.config/MyCardLibrary/ygopro
+if [ ! -e "$ygopro_mc_path" ]; then
+    echo "Please install mycard ygopro!"
+fi
+#download ygopro-deck git repo
+deck_path="$ygopro_mc_path/deck"
+if [[ ! -e "$deck_path/.git" ]]; then
+    rm -rf "$deck_path"
+    cd "$ygopro_mc_path"
+    git clone "git@github.com:fgwsz/ygopro-deck.git"
+    mv "$ygopro_mc_path/ygopro-deck" "$ygopro_mc_path/deck"
+fi
+#pull deck
+cd "$deck_path"
+git pull
+#run ygopro
+cd "$ygopro_mc_path"
+./ygopro
+#push deck
+"$deck_path/push-deck.sh"
--- update-super-pre.sh
+++ update-super-pre.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#download super pre
 root_path=$(dirname "$(readlink -f "$0")")
 super_pre_download_url="https://cdn02.moecube.com:444/ygopro-super-pre/archive/ygopro-super-pre.ypk"
 super_pre_path="$root_path/install/ygopro-super-pre.ypk"
@@ -7,4 +8,16 @@ if [[ -e "$super_pre_path" ]]; then
     rm -rf "$super_pre_path"
 fi
 curl -C - -o "$super_pre_path" "$super_pre_download_url"
-cp -r "${super_pre_path}" "$root_path/ygopro-ocg/expansions/"
+#reset super pre in ygopro-ocg
+ygopro_ocg_path="$root_path/ygopro-ocg"
+cp -r "${super_pre_path}" "$ygopro_ocg_path/expansions/"
+#reset super per in ygopro-mc
+ygopro_mc_path=~/.config/MyCardLibrary/ygopro
+if [ ! -e "$ygopro_mc_path" ]; then
+    echo "Please install mycard ygopro!"
+    exit 1
+fi
+if [ ! -e "$ygopro_mc_path/expansions" ]; then
+    mkdir "$ygopro_mc_path/expansions"
+fi
+cp -r "${super_pre_path}" "$ygopro_mc_path/expansions/"
