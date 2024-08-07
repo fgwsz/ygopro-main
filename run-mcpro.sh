180a628c17d05f15978f8f45b3826524fb90cae9
fgwsz <3191000458@qq.com>
2024-08-07 18:59:11 +0800

    rename

--- run-mcpro.sh
+++ run-mcpro.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
-ygopro_mc_path=~/.config/MyCardLibrary/ygopro
-if [ ! -e "$ygopro_mc_path" ]; then
+mcpro_path=~/.config/MyCardLibrary/ygopro
+#check mycard ygopro
+if [ ! -e "$mcpro_path" ]; then
     echo "Please install mycard ygopro!"
     exit 1
 fi
 #download ygopro-deck git repo
-deck_path="$ygopro_mc_path/deck"
+deck_path="$mcpro_path/deck"
 if [[ ! -e "$deck_path/.git" ]]; then
     rm -rf "$deck_path"
-    cd "$ygopro_mc_path"
+    cd "$mcpro_path"
     git clone "git@github.com:fgwsz/ygopro-deck.git"
-    mv "$ygopro_mc_path/ygopro-deck" "$ygopro_mc_path/deck"
+    mv "$mcpro_path/ygopro-deck" "$mcpro_path/deck"
 fi
 #pull deck
 cd "$deck_path"
 git pull
 #run ygopro
-cd "$ygopro_mc_path"
+cd "$mcpro_path"
 ./ygopro
 #push deck
 "$deck_path/push-deck.sh"
--- run-mycard.sh
+++ run-mycard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 root_path=$(dirname "$(readlink -f "$0")")
+#download mycard-main git repo
 mycard_main_path="$root_path/install/mycard-main"
 if [[ ! -e "$mycard_main_path" ]]; then
     cd "$root_path/install"
@@ -8,18 +9,19 @@ if [[ ! -e "$mycard_main_path" ]]; then
     "$mycard_main_path/install.sh"
     "$mycard_main_path/build.sh"
 fi
-ygopro_mc_path=~/.config/MyCardLibrary/ygopro
-if [ ! -e "$ygopro_mc_path" ]; then
+#check mycard ygopro
+mcpro_path=~/.config/MyCardLibrary/ygopro
+if [ ! -e "$mcpro_path" ]; then
     "$mycard_main_path/run.sh"
     exit 1
 fi
 #download ygopro-deck git repo
-deck_path="$ygopro_mc_path/deck"
+deck_path="$mcpro_path/deck"
 if [[ ! -e "$deck_path/.git" ]]; then
     rm -rf "$deck_path"
-    cd "$ygopro_mc_path"
+    cd "$mcpro_path"
     git clone "git@github.com:fgwsz/ygopro-deck.git"
-    mv "$ygopro_mc_path/ygopro-deck" "$ygopro_mc_path/deck"
+    mv "$mcpro_path/ygopro-deck" "$mcpro_path/deck"
 fi
 #pull deck
 cd "$deck_path"
