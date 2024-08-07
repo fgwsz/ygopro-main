7f32b672d77ff53d52938bbaea864cad82c35b0e
fgwsz <3191000458@qq.com>
2024-08-07 15:52:15 +0800

    clean

--- run-mc.sh
+++ run-mc.sh
@@ -4,6 +4,7 @@ root_path=$(dirname "$(readlink -f "$0")")
 ygopro_mc_path=~/.config/MyCardLibrary/ygopro
 if [ ! -e "$ygopro_mc_path" ]; then
     echo "Please install mycard ygopro!"
+    exit 1
 fi
 #download ygopro-deck git repo
 deck_path="$ygopro_mc_path/deck"
