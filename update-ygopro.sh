1020d3877128620a6a8caabaf34bfe7b290d4522
fgwsz <3191000458@qq.com>
2025-04-10 09:32:02 +0800

    add function of ygopro

--- update-ygopro.sh
+++ update-ygopro.sh
@@ -9,44 +9,30 @@ ygopro_download_url="https://cdn02.moecube.com:444/koishipro/archive/KoishiPro-m
 #check remote ygopro update
 check_update "$ygopro_download_url" "$ygopro_path" "ygopro" "install/ygopro"
 #update install/ygopro
+reset_ygopro(){
+    local name="$1"
+    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-$name/"
+    #reset ext
+    cp -r "$root_path/install/$name-ext"/* "$root_path/ygopro-$name/"
+    #reset deck
+    copy_install_deck "$root_path/ygopro-$name"
+}
 if [ $? -eq 1 ]; then
     download_big_file "$ygopro_download_url" "$ygopro_path"
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-ocg/"
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-408/"
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-2011_11_11/"
-    #reset ext
-    cp -r "$root_path/install/ocg-ext"/* "$root_path/ygopro-ocg/"
-    cp -r "$root_path/install/408-ext"/* "$root_path/ygopro-408/"
-    cp -r "$root_path/install/2011_11_11-ext"/* "$root_path/ygopro-2011_11_11/"
-    #reset deck
     pull_install_deck
-    copy_install_deck "$root_path/ygopro-ocg"
-    copy_install_deck "$root_path/ygopro-408"
-    copy_install_deck "$root_path/ygopro-2011_11_11"
+    reset_ygopro "ocg"
+    reset_ygopro "408"
+    reset_ygopro "2011_11_11"
 fi
-#check ygopro ocg
-if [ ! -e "$root_path/ygopro-ocg/ygopro" ]; then
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-ocg/"
-    #reset ext
-    cp -r "$root_path/install/ocg-ext"/* "$root_path/ygopro-ocg/"
-    #reset deck
-    reset_deck "$root_path/ygopro-ocg"
-fi
-#check ygopro 408
-if [ ! -e "$root_path/ygopro-408/ygopro" ]; then
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-408/"
-    #reset ext
-    cp -r "$root_path/install/408-ext"/* "$root_path/ygopro-408/"
-    #reset deck
-    reset_deck "$root_path/ygopro-408"
-fi
-#check ygopro 2011_11_11
-if [ ! -e "$root_path/ygopro-2011_11_11/ygopro" ]; then
-    tar -zxvf "$ygopro_path" -C "$root_path/ygopro-2011_11_11/"
-    #reset ext
-    cp -r "$root_path/install/2011_11_11-ext"/* "$root_path/ygopro-2011_11_11/"
-    #reset deck
-    reset_deck "$root_path/ygopro-2011_11_11"
-fi
+#check ygopro
+check_and_reset_ygopro(){
+    local name="$1"
+    if [ ! -e "$root_path/ygopro-$name/ygopro" ]; then
+        update_ygopro "$name"
+    fi
+}
+check_and_reset_ygopro "ocg"
+check_and_reset_ygopro "408"
+check_and_reset_ygopro "2011_11_11"
 #update super pre
 "$root_path/update-super-pre.sh"
