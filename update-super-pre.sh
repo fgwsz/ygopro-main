1a099a3a2f282ff17c1e3dad361bba3422c9ebbf
fgwsz <3191000458@qq.com>
2025-04-21 12:32:34 +0800

    修复mcpro无法安装最新super-pre补丁的问题

--- update-super-pre.sh
+++ update-super-pre.sh
@@ -22,10 +22,31 @@ copy_super_pre(){
     check_ygopro "$ygopro_path"
     if [ $? -eq 1 ]; then
         mkdir_expansions "$ygopro_path"
-        cp "$super_pre_path" "$ygopro_ocg_path/expansions/ygopro-super-pre.ypk"
+        cp "$super_pre_path" "$ygopro_path/expansions/ygopro-super-pre.ypk"
         echo "Update $ygopro_path super-pre."
     else
         echo "$ygopro_path hasn't ygopro, please install it."
+    fi
+}
+check_and_copy_super_pre(){
+    local ygopro_path="$1"
+    check_ygopro "$ygopro_path"
+    if [ $? -eq 0 ]; then
+        echo "$ygopro_path hasn't ygopro, please install it."
+        return
+    fi
+    mkdir_expansions "$ygopro_path"
+    local local_path="$ygopro_path/expansions/ygopro-super-pre.ypk"
+    if [ ! -e "$local_path" ]; then
+        cp "$super_pre_path" "$local_path"
+        echo "Update $ygopro_path super-pre."
+        return
+    fi
+    local local_size=$(stat -c%s "$local_path")
+    local install_size=$(stat -c%s "$super_pre_path")
+    if [ "$local_size" -ne "$install_size" ]; then
+        cp "$super_pre_path" "$local_path"
+        echo "Update $ygopro_path super-pre."
     fi
 }
 update_super_pre(){
@@ -40,6 +61,9 @@ update_super_pre(){
         download "$super_pre_download_url" "$super_pre_path"
         copy_super_pre "$ygopro_ocg_path"
         copy_super_pre "$mcpro_path"
+    else
+        check_and_copy_super_pre "$ygopro_ocg_path"
+        check_and_copy_super_pre "$mcpro_path"
     fi
 }
 update_super_pre
