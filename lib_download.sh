3642774454c968fa0b090d6eb63bfc23deea1a5d
fgwsz <3191000458@qq.com>
2025-05-26 11:13:24 +0800

    增加下载前网络检测

--- lib_download.sh
+++ lib_download.sh
@@ -2,33 +2,44 @@
 
 # return 1 has update
 # return 0 has not update
+# return -1 network error
 check_update(){
     local remote_url="$1"
     local local_path="$2"
     local remote_filename="$3"
     local local_filename="$4"
-    local remote_size=""
-    remote_size=$(curl -sI "$remote_url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-    while true; do
-        if [ -z "$remote_size" ]; then
-            echo "Unable to retrieve the size of the $remote_filename."
-            return 1
-        fi
-        remote_size=${remote_size% }
-        if [ -f "$local_path" ]; then
-            local_size=$(stat -c%s "$local_path")
+    local max_retries=3
+    local retry_count=0
+    # 网络重试机制
+    while [ $retry_count -lt $max_retries ]; do
+        remote_size=$(curl -sI "$remote_url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+        if [ $? -eq 0 ]; then
+            break
         else
-            echo "$local_filename does not exist."
-            return 1
-        fi
-        if [ "$remote_size" -eq "$local_size" ]; then
-            echo "$local_filename is up to date."
-            return 0
-        else
-            echo "$local_filename has updates available."
-            return 1
+            echo "Retry Count:($((retry_count+1))/$max_retries)..."
+            ((retry_count++))
+            sleep 5
         fi
     done
+    # 网络错误最终判断
+    if [ $retry_count -eq $max_retries ]; then
+        echo "Network Error:Unable to retrieve the size of the $remote_filename."
+        return -1
+    fi
+    # 文件存在性检查
+    if [ ! -f "$local_path" ]; then
+        echo "$local_filename does not exist."
+        return 1
+    fi
+    # 大小比对
+    local_size=$(stat -c%s "$local_path")
+    if [ "$remote_size" -eq "$local_size" ]; then
+        echo "$local_filename is up to date."
+        return 0
+    else
+        echo "$local_filename has updates available."
+        return 1
+    fi
 }
 download(){
     local remote_url="$1"
